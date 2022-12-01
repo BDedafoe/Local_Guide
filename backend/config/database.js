7dc38e3e8b778fb043ecd360267f6e836e991734
BDedafoe <bmd6260@gmail.com>
2022-11-30 18:54:14 -0600

    sequelize db connected

---
+++ backend/config/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres',
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-
 const app = express();
+const db = require('./config/database');
+
+// Database Test
+db.authenticate()
+    .then(() => console.log('Database connected!'))
+    .catch(err => console.log('Error: ' + err))
 
 app.get ('/', (req, res) =>
-    res.send('Testing! Testing! Testing!'));
+    res.send('Testing! Testing!'));
 
 app.listen(process.env.PORT, console.log(`Server started on ${process.env.PORT}`));
