50538d3cd1240d550e061c08462ba0cfb9e5572b
BDedafoe <bmd6260@gmail.com>
2022-11-30 20:04:30 -0600

    drinks db test success

--- backend/routes/drinks.js
+++ backend/routes/drinks.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Drink = require('../models/Drink')
 
-
+// Get drink list
 router.get('/', (req, res) => 
     Drink.findAll()
         .then(drinks => {
@@ -11,5 +11,29 @@ router.get('/', (req, res) =>
             res.sendStatus(200);
         })
         .catch(err => console.log(err)));
+
+// Add a drink
+router.get('/add', (req, res) => {
+    const data = {
+        place: 'Hop Factory',
+        city: 'Clarksville',
+        state: 'Tennessee',
+        drink_type: 'hoppy beer'
+    }
+
+
+ let { place, city, state, drink_type } = data;
+
+ // instert into table
+  Drink.create({
+    place,
+    city,
+    state,
+    drink_type
+  })
+    .then(Drink => res.redirect('/drinks'))
+    .catch(err => console.log(err));
+
+});
 
 module.exports = router;
