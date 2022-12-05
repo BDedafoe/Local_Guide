0b2ac54d1266192676bf6296162b43eead2aea54
BDedafoe <bmd6260@gmail.com>
2022-12-05 10:23:00 -0600

    added cors to backend

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,31 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser')
-const methodOverride = require('method-override');
 const app = express();
 const db = require('./config/database'); //pgAdmin database connectionghytg
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+const cors = require('cors')
+app.use(cors({
+    origin: 'http://localhost:3007',
+    credentials: true
+}))
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 
 // Database Test
 db.authenticate()
-    .then(() => console.log('Database connected!'))
+    .then(() => console.log('Database Connected!'))
     .catch(err => console.log('Error: ' + err))
 
 //Root Route
 app.get ('/', (req, res) =>
-    res.render('home'));
+    res.send("Connected to the Backend!"));
 
-//Routes
+//Database Routes
 app.use('/drinks', require('./controllers/drinks'));
 app.use('/users', require('./controllers/users'));
 app.use('/foods', require('./controllers/foods'));
 
 //Server
-app.listen(process.env.PORT, console.log(`Server started on ${process.env.PORT}`));
+app.listen(process.env.PORT, console.log(`Server started on Port ${process.env.PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+
 import './App.css';
+import Users from './components/Users';
 
 function App() {
   return (
     <div className="App">
-     <h1>Local's Guide!</h1>
+      <h1>Local's Guide!</h1>
+      <Users />
     </div>
   );
 }
