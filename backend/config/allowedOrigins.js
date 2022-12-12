1ce3ba0a8fad23408a5bef6a1449579076fd0695
BDedafoe <bmd6260@gmail.com>
2022-12-12 10:51:21 -0600

    changed variables for deployment

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3007'
+    'https://local-guide.onrender.com/'
 ]
 
 module.exports = allowedOrigins
--- frontend/src/app/apiSlice.js
+++ frontend/src/app/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials, logOut } from '../auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3007/',
+    baseUrl: 'https://local-guide.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
